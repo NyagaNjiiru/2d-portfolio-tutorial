80ee7e3edcdbfbcd3ab02f99668c7501a9a2ff93
NyagaNjiiru <nyaganjiiru@gmail.com>
2025-07-14 14:43:32 +0300

    Initial commit: 2-d portfolio practice

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my workstation. I do most of my development in JavaScript, Python, and C#, depending on the project. 
+  I'm especially passionate about game development with Godot and GDScript. Here's my <a href="https://github.com/NyagaNjiiru" target="_blank">Github</a>!`,
+  "cs-degree": `This is my BBIT degree. This symbolizes the foundation of my learning. While my journey started in university, my real growth came from consistent practice, late-night builds, and shipping real-world projects.`,
+  "sofa-table": `My creative rest zone. This is where I sketch level ideas, journal progress, or brainstorm mechanics. I also unwind here watching game dev talks and speedruns.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@JSLegendDev" target="_blank">JSLegendDev</a>, <a href="https://www.youtube.com/@PixelArchitect" target="_blank">Pixel Architect</a>,
+  <a href="https://www.youtube.com/@BenBonk" target="_blank">BenBonk</a> (sometimes) and <a href="https://www.youtube.com/@MyGameDevPal" target="_blank">My GameDev Pal</a>!`,
+  bed: `Some of my best design ideas hit when I'm half-asleep. I keep a notebook nearby because game mechanics, code bugs, and worldbuilding ideas always sneak up on me at night.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/NyagaNjiiru/Resume/blob/main/Patrick-Njiru-Nyaga-Resume.pdf" target="_blank">Check it out?</a>
+  Contact me at nyaganjiiru@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. 
+  Check out one of my live web projects: <a href="https://khalkeus.co.ke/" target="_blank">Khalkeus Construction Group</a>.`,
+  library: `My shelves are stacked with books on algorithms, systems design, and game theory. Some are well-read, others… aesthetic choices. 
+  I believe in learning by doing, so that's what I try to stick to.`,
+  exit: `Need to leave? Just close the tab. But if you liked what you saw, let's connect!`,
+};
